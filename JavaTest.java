bdb59919055fd94ebd1db88f37f9e79fac31449e
Bruno <brunonalon2@gmail.com>
2016-06-28 14:27:38 -0600

    4

--- JavaTest.java
+++ JavaTest.java
@@ -2,7 +2,7 @@ public class JavaTest {
 
 	public static void main(String[] args) {
 		System.out.println("Test AutoDeploy2");
-		System.out.println("Test AutoDeploy3");
+		System.out.println("Test AutoDeploy4");
 
 	}
 
